207def19d54ac60d52a50cc08480445fe53d6ee2
Manas Dutta <manasdutta9382@gmail.com>
2025-05-12 17:57:07 +0530

    creating the dashboard ui

---
+++ src/lib/index.ts
@@ -0,0 +1,12 @@
+// place files you want to import through the `$lib` alias in this folder.
+
+export let seo = {
+	title: 'Clara',
+	description:
+		'Clara is a modern startup application template. It is built with SvelteKit, TailwindCSS, and Svelte Animations',
+	image: 'https://i.pinimg.com/736x/85/9a/92/859a92a2629f912010a0a72270aefedc.jpg',
+	twitter: 'SEO twitter',
+	url: 'https://startup-sve.vercel.app',
+	keywords: 'clara, svelte, sveltekit, tailwindcss, svelte animations, startup, template'
+};
+
